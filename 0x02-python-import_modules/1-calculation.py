27b24a8dba907c0f690158868011de2f345c4efb
b1ruk4 <birukwondimubkk@gmail.com>
2022-06-30 11:28:36 -0700

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ = __main__:
+if __name__ == "__main__":
     from calculator_1 import add
     from calculator_1 import sub
     from calculator_1 import mul
-    from claculator_1 import div
+    from calculator_1 import div
 
     a = 10
     b = 5

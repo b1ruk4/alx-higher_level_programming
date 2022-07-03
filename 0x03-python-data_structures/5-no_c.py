b2834db4dfa928174249d7a78c0c8cf42f7707dd
b1ruk4 <birukwondimubkk@gmail.com>
2022-07-03 07:26:12 -0700

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    rm_char = 'c', 'C'
-    my_string = ''.join(i for i in my_string if not i in rm_char)
-    return my_string
+    rm = 'c', 'C'
+    copy = [x for x in my_string if not x in rm]
+    return ("".join(copy))

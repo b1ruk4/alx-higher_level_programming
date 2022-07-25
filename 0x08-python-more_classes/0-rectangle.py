0e14b1f5ff358443e5994a32571a6e820d7faca3
b1ruk4 <birukwondimubkk@gmail.com>
2022-07-25 07:36:00 -0700

    rectangel

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """A Rectangle is a four sided-polygon,
+    having all the internal angles equal to 90 degrees.
+    The two sides at each corner or vertex, meet at right angles.
+    The opposite sides of the rectangle are equal
+    in length which makes it different from a square."""
+    pass

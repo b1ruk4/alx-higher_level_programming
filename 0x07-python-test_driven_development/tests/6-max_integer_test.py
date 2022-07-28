d70e054b4f894beab74aeb023ad4679e54183127
b1ruk4 <birukwondimubkk@gmail.com>
2022-07-28 11:30:40 -0700

    max

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+
+import unittest
+from 6-max_integer import max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_integer(self):
+      """Testes if the function prints the same output"""
+        self.assertAlmostEqual(max_integer[1, 5, 0, 9], 9)
+        self.assertAlmostEqual(max_integer[0, 00, 0, 0], 0)
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    def test_value(self):
+        """testes if what the list can and cann't do"""
+        self.assertRaises(ValueError, 'a')
+        self.assertRaises(ValueError, a)
+    
+
+    def test_type(self):
+        """testes for type of value in list"""
+        self.assertRaises(TypeError, max_integer[2+s, 5j, 0])
+        self.arrestRaises(TypeError, max_integer[a, b, c, None, "and"]
+      
+        

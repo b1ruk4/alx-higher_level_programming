b593ee0202923bfd3355d7d5418866a0cef83296
b1ruk4 <birukwondimubkk@gmail.com>
2022-07-28 14:14:35 -0700

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,11 +6,8 @@ def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
-        raise TypeError('b must be an integer')
-    if a is None or (type(a) is not int and type(a) is not float):
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+
     if type(a) in [float] or type(b) in [float]:
         a = int(round(a, 0))
         b = int(round(b, 0))

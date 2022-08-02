d2263fe1b02da9438cd2bd3c366c005ab2de864f
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-02 12:00:11 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """the function takes filena 
         print the content"""
-    with open(filename, encoding="utf-8)) as myfile:
+    with open("".format(filename), encoding="utf-8)) as myfile:
         print(myfile.read(), end="")

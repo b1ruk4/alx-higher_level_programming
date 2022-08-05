f13fa40829ce52457c43330fb16ffde6ba9ce1c1
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-05 07:33:29 -0700

    readfile fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
+"""
+    File: 0-read_file.py
+    Description: This function reads files.
+    Author: B1ruk4
+    Date: Aug 5 2022
+"""
+
+
 """defining a function read_file"""
 
 
 def read_file(filename=""):
-    """the function takes filen
+    """the function takes file
         print the content
-
-    Args:
+      Args:
         filename(any): name of the file
 
     """
-    with open(filename, encoding="utf-8) as myfile:
+    
+    with open(filename, encoding="utf-8") as myfile:
         print(myfile.read(), end="")

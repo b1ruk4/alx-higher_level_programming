4f1f5fc8a080407ad3eef0c74b823dec3eba377f
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-05 08:04:48 -0700

    fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,11 +6,10 @@ Author: b1ruk4
 Date Created: Aug 5 2022
 """
 
+
 def append_write(filename="", text=""):
     """
     This function appends a string to existing file
     """
-
-
     with open(filename, 'a', encoding="utf8") as myfile:
         return myfile.write(text)

e72930c62f2a31c35b84809655aec26461f5a662
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-05 07:45:26 -0700

    write

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+File: 1-write_file.py
+Desctiption: This function writes to files
+Author: B1ruk4
+Date Created: Aug 5 2022
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    takes a file  and writes to it
+    """
+
+
+    with open(filename, "w", encoding="utf8") as myfile:
+        print(myfile.write(), text, ending="")

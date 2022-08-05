c15be929d89943179c99d5f835c817478665dbc8
b1ruk4 <birukwondimubkk@gmail.com>
2022-08-05 07:23:39 -0700

    readfile fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""defining a function readfile"""
+"""defining a function read_file"""
 
 
 def read_file(filename=""):
     """the function takes filen
-        print the content"""
+        print the content
+
+    Args:
+        filename(any): name of the file
+
+    """
     with open(filename, encoding="utf-8) as myfile:
         print(myfile.read(), end="")
